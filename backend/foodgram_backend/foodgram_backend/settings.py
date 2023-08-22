f8dffccfdeec8a21efc1e084ac139cf614517140
Данил Данилыч <zolotoysnich@yandex.ru>
2023-08-22 12:19:20 +0300

    Добавлено приложение для api

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes.apps.RecipesConfig',
+    'foodgram_api.apps.FoodgramApiConfig',
 ]
 
 MIDDLEWARE = [
